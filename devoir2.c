843c4743e1afdd55d92d2bbfd6e541873222ccaf
Thangbkhn <thangbkhn5691@gmail.com>
2019-10-15 17:51:35 +0700

    OK

--- devoir2.c
+++ devoir2.c
@@ -5,68 +5,177 @@
 #include <string.h>
 #include "devoir2.h"
 
+void swap(char *x, char *y);
+
 char *lettres = "abcdefghijklmnopqrstuvwxz";
 
-char *encrypt(char *message, char *key) {
+//*str is the string, l is the starting index, r is the ending index
+int permute(char *encmessage, char *pattern, char *str, int l, int r)
+{
+    //char *decmessage = malloc(sizemess * sizeof(char));
+    int i;
+    if (l == r){
+        printf("%s\n", str);
+        char *decmessage = decrypt(encmessage, str);     //char *decrypt(char *message, char *key)
+        int ind = search_pattern(pattern, decmessage);    //int search_pattern(char *pattern, char *message)
+        // if(ind) {
+        //   printf("\nFound true key is %s",str );
+        //   printf("\nMessage is %s", decmessage);
+        //  return 0;
+        // }
+        printf("%d\n", ind);
+
+    }
+        
+    else
+    {
+        for (i = l; i <= r; i++)
+        {
+            swap((str + l), (str + i));
+            permute(encmessage,pattern, str, l + 1, r);
+            swap((str + l), (str + i)); //backtrack
+        }
+    }
+}
+
+char *encrypt(char *message, char *key)
+{
   int keylength = strlen(key);
   int messlength = strlen(message);
-  int nrows = ceil(messlength*1.0/keylength);
+  int nrows = ceil(messlength * 1.0 / keylength);
 
   /* allocation d'un tableau pour stocker le message déchiffré */
-  char *tab = malloc((nrows*keylength+1)*sizeof(char));
+  char *tab = malloc((nrows * keylength + 1) * sizeof(char));
 
-  if (!tab) {
+  if (!tab)
+  {
     printf("Erreur d'allocation de mémoire dans encrypt\n");
     exit(-1);
   }
-  
+
   /* remplissage du vecteur selon la règle */
   int i;
   for (i = 0; i < messlength; i++)
-    tab[(i % keylength)*nrows+(i/keylength)] = message[i];
+    tab[(i % keylength) * nrows + (i / keylength)] = message[i];
 
   /* remplissage de la fin aléatoirement */
-  for (; i < nrows*keylength; i++){
+  for (; i < nrows * keylength; i++)
+  {
     //i = 109, 108, 107, 106
-tab[(i % keylength)*nrows+(i/keylength)] = lettres[rand() % 26];
+    tab[(i % keylength) * nrows + (i / keylength)] = lettres[rand() % 26];
   }
-    
+
   tab[i] = '\0';
-  
-  /* rotation des colonnes selon la clé */  
-  for (i = 0; i < keylength; i++) {
-    rotate(tab+i*nrows, (key[i]-'m'), nrows);
+
+  /* rotation des colonnes selon la clé */
+  for (i = 0; i < keylength; i++)
+  {
+    rotate(tab + i * nrows, (key[i] - 'm'), nrows);
   }
   return tab;
+  free(tab);
 }
 
-
-int search_pattern(char *pattern, char *message) {
-  return -1;
+// Permutation
+void swap(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
-char *decrypt(char *message, char *key) {
+int search_pattern(char *pattern, char *message)
+{
+  int pos;
+  char *ptr;
+  ptr = strstr(message, pattern);   //tra ve con tro dau tien cua pattern xuat hien trong message
+
+if (ptr != NULL) // Substring found */
+	{
+    pos = ptr - message;
+		return pos;
+	}
+	else // Substring not found */
+	{
+		return -1;
+	}
+}
+
+//*str is the string, l is the starting index, r is the ending index
+void permute2(char *str, int l, int r)
+{
+    int i;
+    if (l == r)
+        printf("%s\n", str);
+    else
+    {
+        for (i = l; i <= r; i++)
+        {
+            swap((str + l), (str + i));
+            permute2(str, l + 1, r);
+            swap((str + l), (str + i)); //backtrack
+        }
+    }
+}
+
+char *decrypt(char *message, char *key)
+{
+  int keylength = strlen(key);  //p
+  int messlength = strlen(message);   //p*nrows
+  int nrows = ceil(messlength/keylength);
+
+  /* allocation d'un tableau pour stocker le message déchiffré */
+  char *tab = malloc((messlength+1)*sizeof(char));
+   if (!tab) {
+    printf("Erreur d'allocation de mémoire dans encrypt\n");
+    exit(-1);
+  }
+  for (int i = 0; i < keylength; i++) {
+    rotate(message+i*nrows, 'm' - key[i], nrows);
+  }
+  for (int i = 0; i < messlength; i++){
+    tab[i] = message[(i % keylength)*nrows+(i/keylength)];
+  }
+  return tab;
+  free(tab);
+}
+
+char *test_all_key_permutations(char *encmessage, char *pattern, char *key)
+{
+  int lengkey = strlen(key);
+  // permute2(key, 0, lengkey - 1);
+  permute(encmessage, pattern, key, 0, lengkey -1);  //permute(char *encmessage, char *pattern, char *str, int l, int r)
+  // printf(key);
   return NULL;
 }
 
-char *test_all_key_permutations(char *encmessage, char *pattern, char *key) {
-  return NULL;
-}
-
-//n=22
-void rotate(char *arr, int d, int n) { 
-  printf("%s\n", arr);
-char *test = malloc(sizeof(char));
-strncpy(test, arr, n);
-int i;
-char *m = "bcde";
-for (i=0; i< 5; i++) {
-  *(arr+i) = *(m+i);
-}
-printf("%s\n", test);
-// char *m = "bcscdscdc";
-// *arr = *m;
-// *(arr+1) = *(m+1);
-// printf("%d\n", strlen(m));
-// printf("%s\n", arr);
+void rotate(char *arr, int d, int n)
+{
+  char *test = malloc(sizeof(char));
+  // cat thanh tung doan
+  strncpy(test, arr, n);
+  // bat dau ham rotate
+  char *temp = malloc(sizeof(char) * (n));
+  int i;
+  d = d % n;
+  if (d < 0)
+  { // shift right
+    d = d + n;
+  }
+  for (i = 0; i < n; i++)
+  {
+    if (i < d)
+    {
+      *(temp + i) = *(test + (i - d + n));
+    }
+    else
+      *(temp + i) = *(test + (i - d));
+  }
+  for (i = 0; i < 22; i++)
+  {
+    *(arr + i) = *(temp + i);
+  }
+  free(temp);
+  free(test);
 }
--- main2.c
+++ main2.c
@@ -17,7 +17,7 @@ int main() {
   printf("\nDECHIFFREMENT\n----------\n");
   char *decmessage = decrypt(encryptedmess, truekey);
   printf("Message déchiffré:\n%s\n",decmessage);
-  free(decmessage);
+//   free(decmessage);
 
   /* generate and test all keys */
   printf("\nCRYPTANALYSE\n----------------\n");
@@ -33,40 +33,3 @@ int main() {
   exit(0);
 
 }
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// void rotate(char *arr, int d, int n) {
-//     char *arr_rotate = malloc((n+1)*sizeof(char));
-//     d = d % n;
-//     if (d < 0){   // shift right
-//             d = d + n;
-//         }
-//     for (int i = 0; i < n; i ++ ){
-//         if (i < d){
-//         arr_rotate[i] = arr[i - d + n];
-//         }
-//         else
-//             arr_rotate[i] = arr[i - d];
-//         //return arr_rotate;
-//     }
-// arr = arr_rotate;
-// printf("%s\n", arr);
-// printf("***\n");
-// }
-
-// void copy(char *arr) {
-//     char *testarr = malloc(sizeof(char) * 4);
-//     strncpy(testarr, arr, 5);
-//     printf("%s\n", testarr);
-// }
-
-// int main() {
-//     char *arr = "aidjc";
-//     copy(arr);
-
-    
-    
-// }
